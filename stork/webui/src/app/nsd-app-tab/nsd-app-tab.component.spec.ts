10d8cd162bdb403bc06fe3626ff7028200976d43
Piotrek Zadroga <piotrek@isc.org>
2025-03-16 11:57:53 +0100

    new nsd-app-tab component

---
+++ stork/webui/src/app/nsd-app-tab/nsd-app-tab.component.spec.ts
@@ -0,0 +1,217 @@
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing'
+
+import { NsdAppTabComponent } from './nsd-app-tab.component'
+import { RouterTestingModule } from '@angular/router/testing'
+import { TooltipModule } from 'primeng/tooltip'
+import { TabViewModule } from 'primeng/tabview'
+import { MessageService } from 'primeng/api'
+import { LocaltimePipe } from '../pipes/localtime.pipe'
+import { MockLocationStrategy } from '@angular/common/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { of, throwError } from 'rxjs'
+
+import { AppsVersions, Bind9DaemonView, ServicesService, UsersService } from '../backend'
+import { ServerDataService } from '../server-data.service'
+import { RenameAppDialogComponent } from '../rename-app-dialog/rename-app-dialog.component'
+import { DialogModule } from 'primeng/dialog'
+import { FormsModule } from '@angular/forms'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { AppOverviewComponent } from '../app-overview/app-overview.component'
+import { PanelModule } from 'primeng/panel'
+import { PlaceholderPipe } from '../pipes/placeholder.pipe'
+import { ServerSentEventsService, ServerSentEventsTestingService } from '../server-sent-events.service'
+import { EventsPanelComponent } from '../events-panel/events-panel.component'
+import { By } from '@angular/platform-browser'
+import { OverlayPanelModule } from 'primeng/overlaypanel'
+import { DataViewModule } from 'primeng/dataview'
+import { EventTextComponent } from '../event-text/event-text.component'
+import { TableModule } from 'primeng/table'
+import { VersionStatusComponent } from '../version-status/version-status.component'
+import { Severity, VersionService } from '../version.service'
+
+class Daemon {
+    name = 'named'
+    version = '9.18.30'
+}
+
+class Details {
+    daemon: Daemon = new Daemon()
+}
+
+class Machine {
+    id = 1
+}
+
+class App {
+    id = 1
+    name = ''
+    type = 'bind9'
+    machine = new Machine()
+    details = new Details()
+}
+
+class AppTab {
+    app: App = new App()
+}
+
+describe('Bind9AppTabComponent', () => {
+    let component: NsdAppTabComponent
+    let fixture: ComponentFixture<NsdAppTabComponent>
+    let servicesApi: ServicesService
+    let serverData: ServerDataService
+    let versionServiceStub: Partial<VersionService>
+
+    beforeEach(waitForAsync(() => {
+        versionServiceStub = {
+            sanitizeSemver: () => '9.18.30',
+            getCurrentData: () => of({} as AppsVersions),
+            getSoftwareVersionFeedback: () => ({ severity: Severity.success, messages: ['test feedback'] }),
+        }
+
+        TestBed.configureTestingModule({
+            providers: [
+                UsersService,
+                ServicesService,
+                MessageService,
+                MockLocationStrategy,
+                { provide: ServerSentEventsService, useClass: ServerSentEventsTestingService },
+                { provide: VersionService, useValue: versionServiceStub },
+            ],
+            imports: [
+                HttpClientTestingModule,
+                FormsModule,
+                RouterTestingModule,
+                TooltipModule,
+                TabViewModule,
+                DialogModule,
+                NoopAnimationsModule,
+                PanelModule,
+                OverlayPanelModule,
+                DataViewModule,
+                TableModule,
+            ],
+            declarations: [
+                NsdAppTabComponent,
+                LocaltimePipe,
+                PlaceholderPipe,
+                RenameAppDialogComponent,
+                AppOverviewComponent,
+                EventsPanelComponent,
+                EventTextComponent,
+                VersionStatusComponent,
+            ],
+        }).compileComponents()
+    }))
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NsdAppTabComponent)
+        component = fixture.componentInstance
+        servicesApi = fixture.debugElement.injector.get(ServicesService)
+        serverData = fixture.debugElement.injector.get(ServerDataService)
+        fixture.debugElement.injector.get(VersionService)
+        const appTab = new AppTab()
+        component.refreshedAppTab = of(appTab)
+        component.appTab = appTab
+        fixture.detectChanges()
+    })
+
+    it('should create', () => {
+        expect(component).toBeTruthy()
+    })
+
+    it('should display rename dialog', () => {
+        const fakeAppsNames = new Map()
+        spyOn(serverData, 'getAppsNames').and.returnValue(of(fakeAppsNames))
+        const fakeMachinesAddresses = new Set<string>()
+        spyOn(serverData, 'getMachinesAddresses').and.returnValue(of(fakeMachinesAddresses))
+        expect(component.appRenameDialogVisible).toBeFalse()
+        component.showRenameAppDialog()
+        expect(serverData.getAppsNames).toHaveBeenCalled()
+        expect(serverData.getMachinesAddresses).toHaveBeenCalled()
+        // The dialog should be visible after fetching apps names and machines
+        // addresses successfully.
+        expect(component.appRenameDialogVisible).toBeTrue()
+    })
+
+    it('should not display rename dialog when fetching machines fails', () => {
+        const fakeAppsNames = new Map()
+        spyOn(serverData, 'getAppsNames').and.returnValue(of(fakeAppsNames))
+        // Simulate an error while getting machines addresses.
+        spyOn(serverData, 'getMachinesAddresses').and.returnValue(throwError({ status: 404 }))
+        expect(component.appRenameDialogVisible).toBeFalse()
+        component.showRenameAppDialog()
+        expect(serverData.getAppsNames).toHaveBeenCalled()
+        expect(serverData.getMachinesAddresses).toHaveBeenCalled()
+        // The dialog should not be visible because there was an error.
+        expect(component.appRenameDialogVisible).toBeFalse()
+    })
+
+    it('should not display rename dialog when fetching apps fails', () => {
+        // Simulate an error while getting apps names.
+        spyOn(serverData, 'getAppsNames').and.returnValue(throwError({ status: 404 }))
+        const fakeMachinesAddresses = new Set<string>()
+        spyOn(serverData, 'getMachinesAddresses').and.returnValue(of(fakeMachinesAddresses))
+        expect(component.appRenameDialogVisible).toBeFalse()
+        component.showRenameAppDialog()
+        expect(serverData.getAppsNames).toHaveBeenCalled()
+        expect(serverData.getMachinesAddresses).toHaveBeenCalled()
+        // The dialog should not be visible because there was an error.
+        expect(component.appRenameDialogVisible).toBeFalse()
+    })
+
+    it('should send app rename request', () => {
+        // Prepare fake success response to renameApp call.
+        const fakeResponse: any = { data: {} }
+        spyOn(servicesApi, 'renameApp').and.returnValue(of(fakeResponse))
+        // Simulate submitting the app rename request.
+        component.handleRenameDialogSubmitted('bindx@machine3')
+        // Make sure that the request to rename the app was submitted.
+        expect(servicesApi.renameApp).toHaveBeenCalled()
+        // As a result, the app name in the tab should have been updated.
+        expect(component.appTab.app.name).toBe('bindx@machine3')
+    })
+
+    it('should hide app rename dialog', () => {
+        // Show the dialog box.
+        component.appRenameDialogVisible = true
+        fixture.detectChanges()
+        spyOn(servicesApi, 'renameApp')
+        // Cancel the dialog box.
+        component.handleRenameDialogHidden()
+        // Ensure that the dialog box is no longer visible.
+        expect(component.appRenameDialogVisible).toBeFalse()
+        // A request to rename the app should not be sent.
+        expect(servicesApi.renameApp).not.toHaveBeenCalled()
+    })
+
+    it('should include events', fakeAsync(() => {
+        component.ngOnInit()
+        tick()
+        fixture.detectChanges()
+
+        const eventsPanel = fixture.debugElement.query(By.css('app-events-panel'))
+        expect(eventsPanel).toBeTruthy()
+    }))
+
+    it('should display version status component', () => {
+        // One VersionStatus BIND9.
+        let versionStatus = fixture.debugElement.queryAll(By.directive(VersionStatusComponent))
+        expect(versionStatus).toBeTruthy()
+        expect(versionStatus.length).toEqual(1)
+        // Stubbed success icon for BIND 9.18.30 is expected.
+        expect(versionStatus[0].properties.outerHTML).toContain('9.18.30')
+        expect(versionStatus[0].properties.outerHTML).toContain('bind9')
+        expect(versionStatus[0].properties.outerHTML).toContain('text-green-500')
+        expect(versionStatus[0].properties.outerHTML).toContain('test feedback')
+    })
+
+    it('should return 0 when queryHitRatio is undefined', () => {
+        const view = {} as Bind9DaemonView
+        expect(component.getQueryUtilization(view)).toBe(0)
+    })
+
+    it('should calculate correct utilization percentage', () => {
+        const view = { queryHitRatio: 0.756 } as Bind9DaemonView
+        expect(component.getQueryUtilization(view)).toBe(75)
+    })
+})
---
+++ stork/webui/src/app/nsd-app-tab/nsd-app-tab.component.ts
@@ -0,0 +1,312 @@
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core'
+
+import { forkJoin, Observable, Subscription } from 'rxjs'
+
+import { MessageService } from 'primeng/api'
+
+import { ServicesService } from '../backend/api/api'
+import { ServerDataService } from '../server-data.service'
+
+import {
+    durationToString,
+    daemonStatusErred,
+    daemonStatusIconName,
+    daemonStatusIconTooltip,
+    getErrorMessage,
+} from '../utils'
+import { AppTab } from '../apps'
+import { Bind9DaemonView, NSDDaemon } from '../backend'
+
+@Component({
+    selector: 'app-nsd-app-tab',
+    templateUrl: './nsd-app-tab.component.html',
+    styleUrls: ['./nsd-app-tab.component.sass'],
+})
+export class NsdAppTabComponent implements OnInit, OnDestroy {
+    private subscriptions = new Subscription()
+    private _appTab: AppTab
+    @Output() refreshApp = new EventEmitter<number>()
+    @Input() refreshedAppTab: Observable<AppTab>
+
+    daemons: any[] = []
+
+    /**
+     * Active tab index used by the tab view.
+     */
+    activeTabIndex = 0
+
+    /**
+     * Holds a map of existing apps' names and ids.
+     *
+     * The apps' names are used in rename-app-dialog component to validate
+     * the user input.
+     */
+    existingApps: any = []
+
+    /**
+     * Holds a set of existing machines' addresses.
+     *
+     * The machines' addresses are used in rename-app-dialog component to
+     * validate the user input.
+     */
+    existingMachines: any = []
+
+    /**
+     * Controls whether the rename-app-dialog is visible or not.
+     */
+    appRenameDialogVisible = false
+
+    /**
+     * Indicates if a pencil icon was clicked.
+     *
+     * As a result of clicking this icon a dialog box is shown to
+     * rename an app. Loading the dialog box may take a while before
+     * the information about available apps and machines is loaded.
+     * In the meantime, a spinner is shown, indicating that the dialog
+     * box is loading.
+     */
+    showRenameDialogClicked = false
+
+    /**
+     * Event emitter sending an event to the parent component when an app is
+     * renamed.
+     */
+    @Output() renameApp = new EventEmitter<string>()
+
+    constructor(
+        private servicesApi: ServicesService,
+        private serverData: ServerDataService,
+        private msgService: MessageService
+    ) {}
+
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe()
+    }
+
+    /**
+     * Subscribes to the updates of the information about daemons
+     *
+     * The information about the daemons may be updated as a result of
+     * pressing the refresh button in the app tab. In such case, this
+     * component emits an event to which the parent component reacts
+     * and updates the daemons. When the daemons are updated, it
+     * notifies this component via the subscription mechanism.
+     */
+    ngOnInit() {
+        this.subscriptions.add(
+            this.refreshedAppTab.subscribe((data) => {
+                if (data) {
+                    this.initDaemon(data.app.details.nsdaemon)
+                }
+            })
+        )
+    }
+
+    /**
+     * Selects new application tab
+     *
+     * As a result, the local information about the daemons is updated.
+     *
+     * @param appTab pointer to the new app tab data structure.
+     */
+    @Input()
+    set appTab(appTab) {
+        this._appTab = appTab
+        // Refresh local information about the daemon presented by this component.
+        this.initDaemon(appTab.app.details.daemon)
+    }
+
+    /**
+     * Returns information about currently selected app tab.
+     */
+    get appTab(): AppTab {
+        return this._appTab
+    }
+
+    /**
+     * Initializes information about the daemon according to the information
+     * carried in the provided parameter.
+     *
+     * As a result of invoking this function, the view of the component will be
+     * updated.
+     *
+     * @param appTabDaemons information about the daemon stored in the app tab
+     *                      data structure.
+     */
+    private initDaemon(appTabDaemon: NSDDaemon) {
+        const daemonMap = []
+        daemonMap[appTabDaemon.name] = appTabDaemon
+        const DMAP = [['nsd', 'nsd']]
+        const daemons = []
+        for (const dm of DMAP) {
+            if (daemonMap[dm[0]] !== undefined) {
+                daemonMap[dm[0]].niceName = dm[1]
+                daemonMap[dm[0]].statusErred = this.daemonStatusErred(daemonMap[dm[0]])
+                daemons.push(daemonMap[dm[0]])
+            }
+        }
+        this.daemons = daemons
+    }
+
+    /**
+     * An action triggered when refresh button is pressed.
+     */
+    refreshAppState() {
+        this.refreshApp.emit(this._appTab.app.id)
+    }
+
+    /**
+     * Converts duration to pretty string.
+     *
+     * @param duration duration value to be converted.
+     *
+     * @returns duration as text
+     */
+    showDuration(duration) {
+        return durationToString(duration)
+    }
+
+    /**
+     * Get cache effectiveness based on stats for a BIND9 view.
+     *
+     * @param view is a data structure holding the information about the BIND9 view.
+     * @return A percentage is returned as floored int.
+     */
+    getQueryUtilization(view: Bind9DaemonView) {
+        let utilization = 0.0
+        if (!view.queryHitRatio) {
+            return utilization
+        }
+        utilization = 100 * view.queryHitRatio
+        return Math.floor(utilization)
+    }
+
+    /**
+     * Returns boolean value indicating if there is an issue with communication
+     * with the given daemon
+     *
+     * @param daemon data structure holding the information about the daemon.
+     *
+     * @return true if there is a communication problem with the daemon,
+     *         false otherwise.
+     */
+    private daemonStatusErred(daemon): boolean {
+        if (daemon.active && daemonStatusErred(daemon)) {
+            return true
+        }
+        return false
+    }
+
+    /**
+     * Returns the name of the icon to be used when presenting daemon status
+     *
+     * The icon selected depends on whether the daemon is active or not
+     * active and whether there is a communication with the daemon or
+     * not.
+     *
+     * @param daemon data structure holding the information about the daemon.
+     *
+     * @returns ban icon if the daemon is not active, times icon if the daemon
+     *          should be active but the communication with it is broken and
+     *          check icon if the communication with the active daemon is ok.
+     */
+    daemonStatusIconName(daemon) {
+        return daemonStatusIconName(daemon)
+    }
+
+    /**
+     * Returns error text to be displayed when there is a communication issue
+     * with a given daemon
+     *
+     * @param daemon data structure holding the information about the daemon.
+     *
+     * @returns Error text. It includes hints about the communication
+     *          problems when such problems occur, e.g. it includes the
+     *          hint whether the communication is with the agent or daemon.
+     */
+    daemonStatusErrorText(daemon) {
+        return daemonStatusIconTooltip(daemon)
+    }
+
+    /**
+     * Reacts to submitting a new app name from the dialog.
+     *
+     * This function is called when a user presses the rename button in
+     * the app-rename-app-dialog component. It attempts to submit the new
+     * name to the server.
+     *
+     * If the app is successfully renamed, the app name is refreshed in
+     * the app tab view. Additionally, the success message is displayed
+     * in the message service.
+     *
+     * @param event holds new app name.
+     */
+    handleRenameDialogSubmitted(event) {
+        this.appRenameDialogVisible = false
+        this.servicesApi.renameApp(this.appTab.app.id, { name: event }).subscribe(
+            (/* data */) => {
+                // Renaming the app was successful.
+                this.msgService.add({
+                    severity: 'success',
+                    summary: 'App renamed',
+                    detail: 'App successfully renamed to ' + event,
+                })
+                // Let's update the app name in the current tab.
+                this.appTab.app.name = event
+                // Notify the parent component about successfully renaming the app.
+                this.renameApp.emit(event)
+            },
+            (err) => {
+                // Renaming the app failed.
+                const msg = getErrorMessage(err)
+                this.msgService.add({
+                    severity: 'error',
+                    summary: 'Error renaming app',
+                    detail: 'Error renaming app to ' + event + msg,
+                    life: 10000,
+                })
+            }
+        )
+    }
+
+    /**
+     * Reacts to hiding a dialog box for renaming an app.
+     *
+     * This function is called when a dialog box for renaming an app is
+     * closed. It is triggered both in the case when the form is submitted
+     * or cancelled.
+     */
+    handleRenameDialogHidden() {
+        this.appRenameDialogVisible = false
+    }
+
+    /**
+     * Shows a dialog for renaming an app.
+     *
+     * The dialog box component requires a set of machines' addresses
+     * and a map of existing apps' names to validate the new app name.
+     * Therefore, this function attempts to load the machines' addresses
+     * and apps' names prior to displaying the dialog. If it fails, the
+     * dialog box is not displayed.
+     */
+    showRenameAppDialog() {
+        this.showRenameDialogClicked = true
+        forkJoin([this.serverData.getAppsNames(), this.serverData.getMachinesAddresses()]).subscribe(
+            (data) => {
+                this.existingApps = data[0]
+                this.existingMachines = data[1]
+                this.appRenameDialogVisible = true
+                this.showRenameDialogClicked = false
+            },
+            (/* err */) => {
+                this.msgService.add({
+                    severity: 'error',
+                    summary: 'Fetching apps and machines failed',
+                    detail: 'Failed to fetch apps and machines list from the server',
+                    life: 10000,
+                })
+                this.showRenameDialogClicked = false
+            }
+        )
+    }
+}
